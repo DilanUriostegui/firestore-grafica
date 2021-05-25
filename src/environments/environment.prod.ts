44e2600d4530b70835032e5ad93d0a25430d325b
Dilan <dilan.uriostegui@gmail.com>
2021-05-25 16:34:44 -0500

    Fin de la aplicación, crse subio la aplicación aa un servidor de Hosting de Firebase https://firestore-grafica-e0b4b.web.app/inicio

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCxT-DA1VDwDH0Q3nVg28axir0fz_rACps",
+    authDomain: "firestore-grafica-e0b4b.firebaseapp.com",
+    projectId: "firestore-grafica-e0b4b",
+    storageBucket: "firestore-grafica-e0b4b.appspot.com",
+    messagingSenderId: "1039254803295",
+    appId: "1:1039254803295:web:d81d3c5729886261d39cea",
+    measurementId: "G-QJDW49KX65"
+  }
 };
